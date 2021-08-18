1b99a970e9166cd2eafe90ea150ec6b2536fb20f
jagankanipakam <k.jagan321@gmail.com>
2021-08-19 01:14:48 +0530

    updated

---
+++ users/users.controller.js
@@ -0,0 +1,67 @@
+﻿const express = require('express');
+const router = express.Router();
+const userService = require('./user.service');
+
+// routes
+router.post('/authenticate', authenticate);
+router.post('/register', register);
+router.get('/getAll', getAll);
+router.get('/current', getCurrent);
+router.get('/:id', getById);
+router.put('/:id', update);
+router.delete('/:id', _delete);
+router.get('/getAudit/:id', getAudit);
+
+
+module.exports = router;
+
+function authenticate(req, res, next) {
+    userService.authenticate(req.body)
+        .then(user => user ? res.json(user) : res.status(400).json({ message: 'Username or password is incorrect' }))
+        .catch(err => next(err));
+}
+
+function register(req, res, next) {
+    console.log(req.body);
+    userService.create(req.body)
+        .then(() => res.json({}))
+        .catch(err => next(err));
+}
+
+function getAll(req, res, next) {
+    userService.getAll()
+        .then(users =>{
+            return res.json(users)})
+        .catch(err => next(err));
+}
+
+function getAudit(req, res, next) {
+    userService.getAudit(req.params.id)
+        .then(users =>{
+            return res.json(users)})
+        .catch(err => next(err));
+}
+
+function getCurrent(req, res, next) {
+    userService.getById(req.user.sub)
+        .then(user => user ? res.json(user) : res.sendStatus(404))
+        .catch(err => next(err));
+}
+
+function getById(req, res, next) {
+    userService.getById(req.params.id)
+        .then(user => user ? res.json(user) : res.sendStatus(404))
+        .catch(err => next(err));
+}
+
+function update(req, res, next) {
+    userService.update(req.params.id, req.body)
+        .then(() => res.json({}))
+        .catch(err => next(err));
+}
+
+function _delete(req, res, next) {
+    userService.delete(req.params.id)
+        .then(() => res.json({}))
+        .catch(err => next(err));
+}
